38301ef9e091691591bb92fb748e4201a69d2819
Naenaesean <magaud.Sean@gmail.com>
2020-03-22 12:39:33 +0100

    feat(friend button): Setting friend button text

--- facebook/resources/js/views/Users/Show.vue
+++ facebook/resources/js/views/Users/Show.vue
@@ -15,8 +15,11 @@
             </div>
 
 
-            <div class="absolute flex items-center bottom-0 right-0 mb4 mr-12 z-20">
-                <button class="py-1 px-3 bg-grey-400 rounded">Add Friend</button>
+            <div class="absolute flex items-center bottom-0 right-0 mb-4 mr-12 z-20">
+                <button v-if="friendButtonText"
+                        class="py-1 px-3 bg-gray-400 rounded"
+                @click="$store.dispatch('sendFriendRequest', $route.params.userId)">
+                    {{ friendButtonText }}</button>
             </div>
         </div>
 
@@ -62,7 +65,8 @@
 
         computed: {
             ...mapGetters({
-                user: 'user'
+                user: 'user',
+                friendButtonText: 'friendButtonText',
             }),
         }
     }
