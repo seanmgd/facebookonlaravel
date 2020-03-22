78de204beec31309d3ad27a9a9f22cf4ffec6afc
Naenaesean <magaud.Sean@gmail.com>
2020-03-22 16:41:38 +0100

    feat(test, resource): Enhance resource collection and adding new test than post are returned with likes

--- facebook/tests/Feature/LikesTest.php
+++ facebook/tests/Feature/LikesTest.php
@@ -40,4 +40,39 @@ class LikesTest extends TestCase
             ]
         ]);
     }
+
+    public function test_posts_are_returned_with_likes()
+    {
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+        $post = factory(Post::class)->create(['id' => 123, 'user_id' => $user->id]);
+        $this->post('/api/posts/' . $post->id . '/like')
+            ->assertStatus(200);
+
+        $response = $this->get('/api/posts')
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [
+                    [
+                        'data' => [
+                            'type' => 'posts',
+                            'attributes' => [
+                                'likes' => [
+                                  'data' => [
+                                      [
+                                          'data' => [
+                                              'type' => 'likes',
+                                              'like_id' => 1,
+                                              'attributes' => []
+                                          ]
+                                      ]
+                                  ],
+                                    'like_count' => 1,
+                                    'user_likes_post' => true,
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]);
+    }
 }
